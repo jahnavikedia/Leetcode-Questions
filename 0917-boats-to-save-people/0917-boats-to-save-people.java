c329749941d529e8f6b7ed32bd1d2ce48ebdf37f
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2025-04-23 16:48:54 -0700

    Time: 17 ms (24.65%), Space: 54.9 MB (78.77%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.java
+++ 0917-boats-to-save-people/0917-boats-to-save-people.java
@@ -6,33 +6,13 @@ class Solution {
         int count=0;
         while(i<=j && i>=0 && j>=0 && i<n && j<n)
         {
-            if(people[i]==limit)
-            {
-                count++;
-                i++;
-            }
-            if(people[j]==limit)
-            {
-                count++;
-                j--;
-                continue;
-            }
-            if(i==j)
-            {
-                count++;
-                break;
-            }
             if(people[i]+people[j]<=limit)
             {
-                count++;
                 i++;
-                j--;
             }
-            else if(people[i]+people[j]>limit)
-            {
-                count++;
-                j--;
-            }
+            j--;
+            count++;
+            
             
         }
         return count;
