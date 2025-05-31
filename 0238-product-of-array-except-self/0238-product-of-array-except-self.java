f4c2d35dfbe47a708b83ae1b44a608a2b87ad044
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2025-05-31 08:45:37 -0700

    Time: 2 ms (88.98%), Space: 55.8 MB (32.91%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,13 +4,13 @@ class Solution {
         op[0] = 1;
         for(int i=1;i<nums.length;i++)
         {
-            op[i] = nums[i-1]*op[i-1];
+            op[i] = op[i-1]*nums[i-1];
         }
-        int temp =nums[nums.length-1];
+        int temp = nums[nums.length-1];
         for(int i=nums.length-2;i>=0;i--)
         {
-            op[i] = temp*op[i];
-            temp = temp*nums[i];
+            op[i] = temp * op[i];
+            temp = temp * nums[i];
         }
         return op;
     }
