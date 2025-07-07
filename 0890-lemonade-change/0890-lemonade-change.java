b1b0759ee3d14827ffdf29969351214bfc8b1eaf
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2025-07-06 19:48:16 -0700

    Time: 2 ms (93.42%), Space: 55.9 MB (52.22%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0,tens=0;
+        for(int bill:bills)
+        {
+            if(bill == 5) five++;
+            else if(bill == 10)
+            {
+                tens++;
+                if(five>0) five--;
+                else return false;
+            }
+            else
+            {
+                if(tens>0 && five>0)
+                {
+                    tens--;
+                    five--;
+                }
+                else if(five>=3)
+                {
+                    five = five-3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+}
