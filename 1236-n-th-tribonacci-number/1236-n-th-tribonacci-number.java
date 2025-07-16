39f8d728a52496654cdf9a993be69c2a793d369d
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2025-07-16 15:40:48 -0700

    Time: 0 ms (100%), Space: 40.6 MB (27.22%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -1,16 +1,14 @@
 class Solution {
     public int tribonacci(int n) {
-        int t[] = new int[n+1];
-        Arrays.fill(t,-1);
-        int ans = solve(t,n);
-        return ans;
-    }
-    public int solve(int t[], int n)
-    {
         if(n==0) return 0;
         if(n==1 || n==2) return 1;
-        if(t[n] != -1) return t[n];
-        t[n] = solve(t,n-1)+solve(t,n-2)+solve(t,n-3);
+        int t[] = new int[n+1];
+        t[0] = 0; t[1] =1;t[2] =1;
+        for(int i=3;i<=n;i++)
+        {
+            t[i] = t[i-1]+t[i-2]+t[i-3];
+        }
         return t[n];
+
     }
 }
