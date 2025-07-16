f043b50b4103faff6e71fd4c5bd7ae4efbc5c6db
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2025-07-16 15:52:11 -0700

    Time: 0 ms (100%), Space: 40.3 MB (81.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        int t[] = new int[n+1];
-        Arrays.fill(t,-1);
-        int count = solve(n,0,t);
-        return count;
-    }
-    public int solve(int n, int start, int t[])
-    {
-        if(start==n) return 1;
-        if(start > n)return 0;
-        if(t[start]!=-1) return t[start];
-        t[start] = solve(n,start+1,t) + solve(n,start+2,t);
-        return t[start];
+        int t[] = new int[n+2];
+        t[n] = 1;
+        t[n+1] = 0;
+        for(int i=n-1;i>=0;i--)
+        {
+            t[i] = t[i+1]+t[i+2];
+        }
+        return t[0];
     }
 }
