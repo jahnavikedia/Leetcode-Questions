50505b179e033db34379971fb5ae06d2938f1e62
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2025-07-19 16:38:12 -0700

    Time: 0 ms (100%), Space: 40.9 MB (7.21%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,14 +1,13 @@
 class Solution {
     public int integerBreak(int n) {
-        int t[] = new int[n+1];
-        t[1] = 1;
-        for(int i=2;i<=n;i++)
-        {
-            for(int j=1;j<i;j++)
-            {
-                t[i] = Math.max(t[i],Math.max(j*(i-j),j*t[i-j]));
-            }
-        }
-        return t[n];
+       if(n==2) return 1;
+       if(n==3) return 2;
+       int result = 1;
+       while(n>4)
+       {
+        result*=3;
+        n-=3;
+       } 
+       return result*n;
     }
 }
