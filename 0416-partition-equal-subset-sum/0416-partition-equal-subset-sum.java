ed18300fe44344d67b25837b6394c9d003dc7b3a
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2025-07-19 01:42:19 -0700

    Time: 30 ms (74.86%), Space: 42.5 MB (78.08%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -8,18 +8,15 @@ class Solution {
     private boolean subsetSum(int nums[],int sum)
     {
         int n = nums.length;
-        boolean t[][] = new boolean[n+1][sum+1];
-        for(int j=0;j<=sum;j++)t[0][j] = false;
-        for(int i=0;i<=n;i++) t[i][0] = true;
-        for(int i=1;i<=n;i++)
+        boolean t[] = new boolean[sum+1];
+        t[0] = true;
+        for(int num:nums)
         {
-            for(int j=1;j<=sum;j++)
+            for(int i=sum;i>=num;i--)
             {
-                if(nums[i-1]<=j)
-                t[i][j] = t[i-1][j] || t[i-1][j-nums[i-1]];
-                else t[i][j] = t[i-1][j];
+                t[i] = t[i] || t[i-num];
             }
         }
-        return t[n][sum];
+        return t[sum];
     }
 }
