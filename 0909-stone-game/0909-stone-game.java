c412e88b362469d9455cd21cdbff1d57faf3bc3a
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2025-07-22 11:03:24 -0700

    Time: 7 ms (32.41%), Space: 44.8 MB (18.92%) - LeetHub

--- 0909-stone-game/0909-stone-game.java
+++ 0909-stone-game/0909-stone-game.java
@@ -1,6 +1,18 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
-        
+        int n = piles.length;
+        int dp[][] = new int[n][n];
+        for(int i=0;i<n;i++) dp[i][i] = piles[i];
+        for(int i=2;i<=n;i++)
+        {
+            for(int left = 0;left<=n-i;left++)
+            {
+                int right = left+i-1;
+                int pickLeft = piles[left] - dp[left+1][right];
+                int pickRight = piles[right] - dp[left][right-1];
+                dp[left][right] = Math.max(pickLeft,pickRight); 
+            }
+        }
+        return dp[0][n-1]>0;
     }
 }
