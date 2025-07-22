68bbc21861572bf38131068af959630a38756015
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2025-07-22 11:36:38 -0700

    Time: 5 ms (59.69%), Space: 45.3 MB (18.42%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,27 +1,18 @@
 class Solution {
     public int minDistance(String a, String b) {
-        int m = a.length(), n = b.length();
+        if(a.equals(b)) return 0;
+        int m = a.length();
+        int n = b.length();
         int t[][] = new int[m+1][n+1];
-        for(int i=0;i<m+1;i++)
+        for(int i=0;i<=m;i++)t[i][0] = i;
+        for(int j=0;j<=n;j++) t[0][j] = j;
+        for(int i=1;i<=m;i++)
         {
-            t[i][0] = i;
-        }
-         for(int j=0;j<n+1;j++)
-        {
-            t[0][j] = j;
-        }
-        for(int i=1;i<m+1;i++)
-        {
-            for(int j=1;j<n+1;j++)
+            for(int j=1;j<=n;j++)
             {
-                if(a.charAt(i-1)==b.charAt(j-1))
-                {
-                    t[i][j] = t[i-1][j-1];
-                }
+                if(a.charAt(i-1)==b.charAt(j-1)) t[i][j] = t[i-1][j-1];
                 else
-                {
-                    t[i][j] = 1+Math.min(t[i-1][j],Math.min(t[i][j-1],t[i-1][j-1]));
-                }
+                    t[i][j] = 1+ Math.min(t[i][j-1],Math.min(t[i-1][j],t[i-1][j-1]));
             }
         }
         return t[m][n];
