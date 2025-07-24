78972ae3fdb73b2cec9793495811957d20a95fac
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2025-07-24 14:21:23 -0700

    Time: 8 ms (27.42%), Space: 45.8 MB (26.85%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -8,25 +8,23 @@ class Solution {
             adj.get(pre[1]).add(pre[0]);
             in[pre[0]]++;
         }
-        
         Queue<Integer> q = new LinkedList<>();
         for(int i=0;i<n;i++)
         {
             if(in[i]==0)q.offer(i);
         }
+        int count=0;
         while(!q.isEmpty())
         {
             int f = q.poll();
+            count++;
             for(int nbr : adj.get(f))
             {
                 in[nbr]--;
                 if(in[nbr] ==0) q.offer(nbr);
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            if(in[i]!=0)return false;
-        }
-        return true;
+        //for(int i=0;i<n;i++)if(in[i]!=0)return false;return true;
+        return count==n;
     }
 }
