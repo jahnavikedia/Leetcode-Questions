9b4c4e93ca04b644030fc8e83657e880d88c4267
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2025-07-24 14:19:47 -0700

    Time: 7 ms (45.33%), Space: 46.2 MB (8.98%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canFinish(int n, int[][] prereq) {
+        Map<Integer,List<Integer>> adj = new HashMap<>();
+        for (int i = 0; i < n; i++) adj.put(i, new ArrayList<>());
+        int in[] = new int[n];
+        for(int[] pre : prereq)
+        {
+            adj.get(pre[1]).add(pre[0]);
+            in[pre[0]]++;
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++)
+        {
+            if(in[i]==0)q.offer(i);
+        }
+        while(!q.isEmpty())
+        {
+            int f = q.poll();
+            for(int nbr : adj.get(f))
+            {
+                in[nbr]--;
+                if(in[nbr] ==0) q.offer(nbr);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(in[i]!=0)return false;
+        }
+        return true;
+    }
+}
