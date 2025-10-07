c5b313c20b41356830e03f09a2241e11825efe32
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2025-10-07 15:15:44 -0700

    Time: 156 ms (8.68%), Space: 45.7 MB (6.78%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n = nums.length;
         Arrays.sort(nums);
-        Set<List<Integer>> op = new HashSet<>();
+        Set<List<Integer>> set = new HashSet<>();
+        int n = nums.length;
         for(int i=0;i<n;i++)
         {
             for(int j=i+1;j<n;j++)
             {
-                int k =j+1;
+                int k=j+1;
                 int l = n-1;
                 while(k<l)
                 {
                     long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
                     if(sum==target)
                     {
-                        op.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        set.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
                         k++;
                         l--;
                     }
@@ -23,6 +23,6 @@ class Solution {
                 }
             }
         }
-        return new ArrayList<>(op);
+        return new ArrayList<>(set);
     }
 }
