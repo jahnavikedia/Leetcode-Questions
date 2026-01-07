556c5ac5b4dcca13c0d82f488c619db6a4549eaa
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-01-06 20:44:01 -0800

    Time: 11 ms (74.23%), Space: 19.4 MB (7.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
         mp = {}
         for c in s:
             mp[c] = mp.get(c,0)+1
         for c in t:
-            if c not in mp : return False
+            if c not in mp or mp[c] == 0: return False
             mp[c] = mp[c] - 1
-        for k in mp:
-            if mp[k] != 0: return False 
         return True
