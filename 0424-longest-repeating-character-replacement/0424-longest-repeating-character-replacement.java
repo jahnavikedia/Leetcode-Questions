ac7981db8d7f140fb3252672cbe04d3c8d3529fe
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-14 16:41:06 -0700

    Time: 25 ms (32.64%), Space: 47.2 MB (22.25%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,31 +1,31 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int i=0,j=0,max = -1,maxFreq=-1;
-        Map<Character,Integer> map = new HashMap<>();
-        while(j<s.length())
+        Map<Character, Integer> map = new HashMap<>();
+        int maxCount = -1, max=-1;
+        int n = s.length(), i=0, j=0;
+        while(j<n)
         {
             char c = s.charAt(j);
             map.put(c,map.getOrDefault(c,0)+1);
-            maxFreq = Math.max(maxFreq, map.get(c));
-            if((j-i+1)-maxFreq <= k)
+            maxCount = Math.max(maxCount,map.get(c));
+            if((j-i+1)-maxCount <= k)
             {
                 max = Math.max(max,j-i+1);
-                j++;
             }
-            else if((j-i+1) - maxFreq > k)
+            else
             {
-                while((j-i+1) - maxFreq > k)
+                while((j-i+1)-maxCount > k)
                 {
-                    char temp = s.charAt(i);
-                    map.put(temp,map.get(temp)-1);
+                    char d = s.charAt(i);
+                    map.put(d, map.get(d)-1);
                     i++;
-                    if((j-i+1)-maxFreq == k)
+                    if((j-i+1)-maxCount ==k)
                     {
-                        max = Math.max(max,j-i+1);
+                        max = Math.max(max, j-i+1);
                     }
                 }
-                j++;
             }
+            j++;
         }
         return max;
     }
