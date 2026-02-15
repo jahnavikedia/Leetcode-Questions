b492e0ceea0d4d5db531b5963e2a11d24765d451
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-14 17:02:13 -0700

    Time: 29 ms (27.36%), Space: 45.1 MB (33.91%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,43 +1,42 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length()>s2.length()) return false;
+        int m = s1.length(), n = s2.length();
+        if(m>n) return false;
         if(s1.equals(s2)) return true;
-        int j=0,count=0; 
         Map<Character,Integer> map = new HashMap<>();
-        for(int i=0;i<s1.length();i++)
+        int count = 0;
+        for(int i=0;i<m;i++)
         {
-            if(map.containsKey(s1.charAt(i)))
-            {
-                map.put(s1.charAt(i),map.get(s1.charAt(i))+1);
-            }
+            char c = s1.charAt(i);
+            if(map.containsKey(c)) map.put(c,map.get(c)+1);
             else
             {
-                map.put(s1.charAt(i),1);
+                map.put(c,1);
                 count++;
             }
         }
-        int i=0,k = s1.length();
-        while(j<s2.length())
+        int i=0,j=0;
+        while(j<n)
         {
-            char ch = s2.charAt(j);
-            if(map.containsKey(ch))
+            char c = s2.charAt(j);
+            if(map.containsKey(c))
             {
-                map.put(ch,map.get(ch)-1);
-                if(map.get(ch)==0) count--;
+                map.put(c, map.get(c)-1);
+                if(map.get(c)==0) count--;
             }
-            if(j-i+1 == k)
+            if(j-i+1 == m)
             {
-                if(count==0) return true;
-                if(map.containsKey(s2.charAt(i)))
+                if(count == 0) return true;
+                char ch = s2.charAt(i);
+                if(map.containsKey(ch))
                 {
-                    if(map.get(s2.charAt(i))==0) count++;
-                    map.put(s2.charAt(i),map.get(s2.charAt(i))+1);
+                    if(map.get(ch)==0) count++;
+                    map.put(ch, map.get(ch)+1);
                 }
                 i++;
-            }
+            } 
             j++;
         }
         return false;
-
     }
 }
