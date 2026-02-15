87ee2fa9a2ebf40f3491ac3857f32d9a3e579c0c
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-14 19:20:59 -0700

    Time: 6 ms (14.45%), Space: 47.3 MB (27.3%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,6 +1,6 @@
 class MinStack {
-    public Stack<Long> stack = new Stack<>();
     public static long minEle;
+    public Stack<Long> stack = new Stack<>();
     public MinStack() {
         
     }
@@ -11,32 +11,32 @@ class MinStack {
             stack.push(val);
             minEle = val;
         }
-        else if(val>=minEle) stack.push(val);
-        else
-        {
-            stack.push(2*val-minEle);
-            minEle = val;
+        else{
+            if(val<minEle)
+            {
+                stack.push(2*val - minEle);
+                minEle = val;
+            }
+            else{
+                stack.push(val);
+            }
         }
     }
     
     public void pop() {
-       if(stack.isEmpty()) return;
-       else
-       {
-        if(stack.peek()>=minEle) stack.pop();
-        else 
+        if(stack.peek()<minEle)
         {
-            minEle = 2*minEle - stack.pop();
+            minEle = 2 * minEle - stack.pop();
         }
-       } 
+        else{
+            stack.pop();
+        }
     }
     
     public long top() {
-        if(stack.isEmpty()) return -1;
-        else
-        {
-            if(stack.peek()>=minEle) return stack.peek();
-            else return minEle;
+        if(stack.peek()<minEle) return minEle;
+        else{
+            return stack.peek();
         }
     }
     
