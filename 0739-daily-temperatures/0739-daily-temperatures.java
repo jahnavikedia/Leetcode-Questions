c27492d7e13cc5071f375290236c89e3e7ab2bf0
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-14 17:30:07 -0700

    Time: 89 ms (5.28%), Space: 97.6 MB (97.79%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,41 +1,30 @@
-public class Pair{
+class Pair
+{
     int ele;
     int index;
-    Pair(int ele, int index){
+    Pair(int ele, int index)
+    {
         this.ele = ele;
         this.index = index;
     }
 }
 class Solution {
-    public int[] dailyTemperatures(int[] arr) {
-        int op[] = new int[arr.length];
-        Stack<Pair> stack = new Stack<>();
-        for(int i=arr.length-1;i>=0;i--)
+    public int[] dailyTemperatures(int[] temp) {
+        int n = temp.length;
+        Stack<Pair> stack = new Stack<Pair>();
+        int op[] = new int[n];
+        for(int i=n-1;i>=0;i--)
         {
-            if(stack.isEmpty())
+            if(stack.isEmpty()) op[i] = 0;
+            else if(stack.peek().ele>temp[i]) op[i] = stack.peek().index - i;
+            else
             {
-                op[i] = 0;
+                while(!stack.isEmpty() && stack.peek().ele<=temp[i]) stack.pop();
+                if(stack.isEmpty()) op[i] = 0;
+                else op[i] = stack.peek().index - i;
             }
-            else if(arr[i]<stack.peek().ele)
-            {
-                op[i] = stack.peek().index - i;
-            }
-            else if(arr[i]>=stack.peek().ele)
-            {
-                while(!stack.isEmpty() && arr[i]>=stack.peek().ele)
-                {
-                    stack.pop();
-                }
-                if(stack.isEmpty())
-                {
-                    op[i] = 0;
-                }
-                else{
-                    op[i] = stack.peek().index - i;
-                }
-            }
-            stack.push(new Pair(arr[i],i));
+            stack.push(new Pair(temp[i],i));
         }
-        return op; 
+        return op;
     }
 }
