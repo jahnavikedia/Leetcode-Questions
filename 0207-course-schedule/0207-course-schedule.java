e313c6ace2a0a20d84086c0261bce48d9bf0d459
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-18 13:42:22 -0700

    Time: 9 ms (22.52%), Space: 47.1 MB (65.49%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,30 +1,26 @@
 class Solution {
-    public boolean canFinish(int n, int[][] prereq) {
-        Map<Integer,List<Integer>> adj = new HashMap<>();
-        for (int i = 0; i < n; i++) adj.put(i, new ArrayList<>());
+    public boolean canFinish(int n, int[][] preReq) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for(int i=0;i<n;i++) adj.put(i,new ArrayList<>());
         int in[] = new int[n];
-        for(int[] pre : prereq)
+        for(int pre[]:preReq)
         {
             adj.get(pre[1]).add(pre[0]);
             in[pre[0]]++;
         }
         Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<n;i++)
-        {
-            if(in[i]==0)q.offer(i);
-        }
+        for(int i=0;i<n;i++) if(in[i]==0) q.offer(i);
         int count=0;
         while(!q.isEmpty())
         {
-            int f = q.poll();
             count++;
-            for(int nbr : adj.get(f))
+            int f = q.poll();
+            for(int nbr:adj.get(f))
             {
                 in[nbr]--;
-                if(in[nbr] ==0) q.offer(nbr);
+                if(in[nbr]==0) q.offer(nbr);
             }
         }
-        //for(int i=0;i<n;i++)if(in[i]!=0)return false;return true;
         return count==n;
     }
 }
