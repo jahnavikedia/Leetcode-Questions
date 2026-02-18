a0d892aa272f70612c674c6121742ede4e5f93f4
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-18 12:04:49 -0700

    Time: 20 ms (73.23%), Space: 109.8 MB (26.77%) - LeetHub

--- 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.java
+++ 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.java
@@ -8,15 +8,16 @@ class Solution {
         {
             for(int j=0;j<n;j++)
             {
-                if(i==0 && j==0){
+                if(i==0 && j==0)
+                {
                     minSoFar[i][j] = grid.get(i).get(j);
                     continue;
                 }
                 int minPrev = Integer.MAX_VALUE;
-                if(i>0) minPrev = Math.min(minPrev,minSoFar[i-1][j]);
+                if(i>0) minPrev = Math.min(minPrev, minSoFar[i-1][j]);
                 if(j>0) minPrev = Math.min(minPrev, minSoFar[i][j-1]);
-                ans = Math.max(ans, grid.get(i).get(j) - minPrev);
-                minSoFar[i][j] = Math.min(grid.get(i).get(j),minPrev);
+                ans = Math.max(ans, grid.get(i).get(j)-minPrev);
+                minSoFar[i][j] = Math.min(minPrev, grid.get(i).get(j));
             }
         }
         return ans;
