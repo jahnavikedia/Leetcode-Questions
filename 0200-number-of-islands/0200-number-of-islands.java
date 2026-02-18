d9ccd8cbb84ab97f8e7beda78097b9284029a982
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-18 14:47:54 -0700

    Time: 3 ms (87.37%), Space: 52.8 MB (14.65%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,32 +1,31 @@
 class Solution {
-    public int dir[][] = {{0,-1},{1,0},{0,1},{-1,0}};
+    public int dir[][] = {{-1,0},{0,-1},{1,0},{0,1}};
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int count =0;
+        int count=0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]=='1')
                 {
-                    dfs(grid,i,j,m,n);
+                    dfs(grid,m,n,i,j);
                     count++;
                 }
             }
         }
         return count;
     }
-
-    public void dfs(char[][] grid, int i, int j, int m, int n)
-    {
-        grid[i][j] = '0';
-        for(int d[] : dir)
+        public void dfs(char grid[][], int m, int n, int i, int j)
         {
-            int x = i + d[0];
-            int y = j + d[1];
-            if(x>=0 && y>=0 && x<m && y<n && grid[x][y]=='1') 
-                dfs(grid,x,y,m,n);
+            grid[i][j]='0';
+            for(int d[] : dir)
+            {
+                int x = d[0]+i;
+                int y = d[1]+j;
+                if(x>=0&&x<m&&y>=0&&y<n && grid[x][y]!='0')
+                    dfs(grid,m,n,x,y);
+            }
         }
-    }
 }
