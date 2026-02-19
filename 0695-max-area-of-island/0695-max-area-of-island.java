3b58ecb84e7dfc4fee1c0cc9e8bcbae51eab0ba2
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-18 17:19:17 -0700

    Time: 2 ms (67.52%), Space: 46.5 MB (69.28%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,31 +1,34 @@
 class Solution {
-    public int dirs[][] = {{0,1},{-1,0},{0,-1},{1,0}};
+    public int dir[][] = {{0,1},{0,-1},{1,0},{-1,0}};
     public int maxAreaOfIsland(int[][] grid) {
         int m = grid.length;
         int n= grid[0].length;
-        int max=0;
+        int max = Integer.MIN_VALUE;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==1)
                 {
-                    max = Math.max(max, dfs(grid,i,j,m,n));
+                    max=Math.max(max,dfs(grid,i,j,m,n));
                 }
             }
         }
-        return max;
+        return Math.max(max, 0);
     }
 
     public int dfs(int grid[][], int i, int j, int m, int n)
     {
-        grid[i][j] = 0;
-        int sum = 1;
-        for(int dir[]: dirs)
+        grid[i][j]=0;
+        int sum=1;
+        for(int d[]:dir)
         {
-            int x = dir[0]+i;
-            int y = dir[1]+j;
-            if(x>=0 && x<m && y>=0 && y<n && grid[x][y]==1) sum+=dfs(grid,x,y,m,n);
+            int x = d[0]+i;
+            int y = d[1]+j;
+            if(x>=0 && x<m && y>=0 && y<n && grid[x][y]==1)
+            {
+                 sum+=dfs(grid,x,y,m,n);
+            }
         }
         return sum;
     }
