dc73838402ecfa5939442b46dc59a243ae4ee9a5
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-19 13:24:31 -0700

    Time: 84 ms (5.23%), Space: 97.6 MB (98.47%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,22 +1,21 @@
-class Pair
-{
-    int ele;
-    int index;
-    Pair(int ele, int index)
-    {
-        this.ele = ele;
-        this.index = index;
-    }
-}
 class Solution {
+    class Pair{
+        int ele, index;
+        Pair(int ele, int index)
+        {
+            this.ele=ele;
+            this.index = index;
+        }
+    }
     public int[] dailyTemperatures(int[] temp) {
+        if(temp.length==1) return new int[]{0};
         int n = temp.length;
-        Stack<Pair> stack = new Stack<Pair>();
+        Stack<Pair> stack = new Stack<>();
         int op[] = new int[n];
         for(int i=n-1;i>=0;i--)
         {
             if(stack.isEmpty()) op[i] = 0;
-            else if(stack.peek().ele>temp[i]) op[i] = stack.peek().index - i;
+            else if(stack.peek().ele>temp[i]) op[i] = stack.peek().index-i;
             else
             {
                 while(!stack.isEmpty() && stack.peek().ele<=temp[i]) stack.pop();
