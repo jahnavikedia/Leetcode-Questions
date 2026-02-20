2df7e7b3c8a81ca95f03f190efc6856938def45a
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-19 20:00:01 -0700

    Time: 1 ms (85.35%), Space: 43.5 MB (26.19%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0 || s.charAt(0) == '0') return 0;
         int n = s.length();
+        if(s.charAt(0)=='0') return 0;
         int t[] = new int[n+1];
         t[0] = 1;
         t[1] = s.charAt(0)=='0'?0:1;
@@ -9,10 +9,9 @@ class Solution {
         {
             int one = Integer.valueOf(s.substring(i-1,i));
             int two = Integer.valueOf(s.substring(i-2,i));
-            if(one != 0) t[i] += t[i-1];
-            if(two >=10 && two<=26) t[i] += t[i-2]; 
+            if(one > 0) t[i]+=t[i-1];
+            if(two>=10 && two<=26) t[i]+=t[i-2];
         }
         return t[n];
-
     }
 }
