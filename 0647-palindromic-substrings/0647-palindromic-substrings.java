b23a660ebbeda6f09d9f78daa185f5100a00b785
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-19 20:34:21 -0700

    Time: 6 ms (64.35%), Space: 42.7 MB (92.23%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,24 +1,24 @@
 class Solution {
     public int countSubstrings(String s) {
+        int n = s.length();
+        if(n==1) return 1;
         int count=0;
-        for(int i=0;i<s.length();i++)
+        for(int i=0;i<n;i++)
         {
             int odd = expand(s,i,i);
             int even = expand(s,i,i+1);
-            count = count+odd+even; 
+            count+=odd+even;
         }
         return count;
     }
-    private int expand(String s, int left, int right)
+
+    public int expand(String s, int left, int right)
     {
-        int count=0;
-        while(left>=0 && right<s.length() && s.charAt(left) == s.charAt(right))
+        int count =0;
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right))
         {
-            left--;
-            right++;
-            count++;
+            left--;right++;count++;
         }
         return count;
-        
     }
 }
