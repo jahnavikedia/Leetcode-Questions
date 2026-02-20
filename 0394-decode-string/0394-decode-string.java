e8589ae26d77726c9287fdf178e74c8cd3d62d68
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-19 22:52:43 -0700

    Time: 3 ms (20.97%), Space: 42.9 MB (54.56%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -3,17 +3,16 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         for(char ch: s.toCharArray())
         {
-            if(ch!=']') stack.push(ch);
-            else
-            {
+            if(ch!=']')stack.push(ch);
+            else{
                 StringBuilder substr = new StringBuilder();
                 while(stack.peek()!='[') substr.insert(0,stack.pop());
                 stack.pop();
                 StringBuilder k = new StringBuilder();
-                while(!stack.isEmpty()&&Character.isDigit(stack.peek())) k.insert(0,stack.pop());
+                while(!stack.isEmpty() && Character.isDigit(stack.peek())) k.insert(0,stack.pop());
                 int repeat = Integer.parseInt(k.toString());
                 String repeated = substr.toString().repeat(repeat);
-                for(char c:repeated.toCharArray()) stack.push(c);
+                for(char c: repeated.toCharArray()) stack.push(c);
             }
         }
         StringBuilder result = new StringBuilder();
