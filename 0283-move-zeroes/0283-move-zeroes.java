e971524bad3e2f6c3f8af27be8e5702fae2d3aec
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-02-24 22:01:02 -0700

    Time: 1 ms (99.77%), Space: 48 MB (36.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,11 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int n = nums.length;
         int insertPos=0;
         for(int num:nums)
         {
-            if(num!=0)nums[insertPos++]=num;
+            if(num!=0)nums[insertPos++] = num;
         }
-        while(insertPos<nums.length)
-        {
-            nums[insertPos++] = 0;
-        }
+        while(insertPos<n)nums[insertPos++] = 0;
     }
 }
