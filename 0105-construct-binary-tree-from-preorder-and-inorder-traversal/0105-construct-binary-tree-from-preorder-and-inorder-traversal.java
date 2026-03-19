829092edef4c7e7e0cccaf46af208655571ac344
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-03-19 12:49:16 -0700

    Time: 3 ms (30.21%), Space: 45.6 MB (77.86%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,21 +14,24 @@
  * }
  */
 class Solution {
-    public TreeNode buildTree(int[] preOrder, int[] inOrder) {
-        Map<Integer,Integer> inMap = new HashMap<>();
-        for(int i=0;i<inOrder.length;i++)
-            inMap.put(inOrder[i],i);
-        TreeNode root = buildTreeHelper(preOrder, 0, preOrder.length-1, inOrder, 0, inOrder.length-1, inMap);
-        return root;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+       return build(preorder, inorder, 0, 0, inorder.length-1); 
     }
-    public TreeNode buildTreeHelper(int preOrder[], int preStart, int preEnd, int inOrder[], int inStart, int inEnd, Map<Integer,Integer> inMap)
+    public TreeNode build(int pre[], int in[], int preStart, int inStart, int inEnd)
     {
-        if(preStart>preEnd || inStart > inEnd) return null;
-        TreeNode root = new TreeNode(preOrder[preStart]);
-        int inRoot = inMap.get(root.val);
-        int left = inRoot - inStart;
-        root.left = buildTreeHelper(preOrder, preStart+1, preStart + left, inOrder, inStart, inRoot -1, inMap);
-        root.right = buildTreeHelper(preOrder, preStart+left+1, preEnd, inOrder, inRoot+1, inEnd, inMap);
+        if(preStart>pre.length-1 || inStart>inEnd) return null;
+        TreeNode root = new TreeNode(pre[preStart]);
+        int mid=0;
+        for(int i=inStart;i<=inEnd;i++)
+        {
+            if(in[i]==root.val)
+            {
+                mid = i; break;
+            }
+        }
+        int leftSize = mid-inStart;
+        root.left = build(pre, in, preStart+1,inStart, mid-1);
+        root.right = build(pre, in, preStart+1+leftSize, mid+1, inEnd);
         return root;
     }
 }
