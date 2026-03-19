e5cb19975d5474f8abee42b4dcc0d0ca73ecfb68
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-03-18 20:10:47 -0700

    Time: 21 ms (42.7%), Space: 45.8 MB (58.52%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,28 +1,30 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> op = new ArrayList<>();
         int n = nums.length;
-        for(int i=0;i<n;i++)
+        Arrays.sort(nums);
+        for(int i=0;i<n-3;i++)
         {
-            for(int j=i+1;j<n;j++)
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n-2;j++)
             {
-                int k=j+1;
-                int l = n-1;
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1, l=n-1;
                 while(k<l)
                 {
                     long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
                     if(sum==target)
                     {
-                        set.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                        k++;
-                        l--;
+                        op.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        while(k<l && nums[k]==nums[k+1]) k++;
+                        while(k<l && nums[l]==nums[l-1]) l--;
+                        k++;l--;
                     }
                     else if(sum<target) k++;
                     else l--;
                 }
             }
         }
-        return new ArrayList<>(set);
+        return op;
     }
 }
