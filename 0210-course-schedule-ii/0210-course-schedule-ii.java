7163b0488cd5ecd2ec34ff6982e673e4f84d4e2a
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-03-20 09:17:29 -0700

    Time: 7 ms (34.71%), Space: 48.3 MB (11.69%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,34 +1,30 @@
 class Solution {
     public int[] findOrder(int n, int[][] preReq) {
-        ArrayList<Integer> op = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
         Map<Integer,List<Integer>> adj = new HashMap<>();
         int in[] = new int[n];
         for(int i=0;i<n;i++)adj.put(i,new ArrayList<>());
-        for(int pre[] : preReq)
-        {
+        for(int pre[] : preReq){
             adj.get(pre[1]).add(pre[0]);
             in[pre[0]]++;
-        }
-        for(int i=0;i<n;i++)if(in[i]==0)q.offer(i);
+        } 
+        Queue<Integer> q = new LinkedList<>();
         int count=0;
+        for(int i=0;i<n;i++)if(in[i]==0) q.offer(i);
+        List<Integer> op = new ArrayList<>();
         while(!q.isEmpty())
         {
             count++;
             int f = q.poll();
             op.add(f);
-            for(int nbr : adj.get(f))
+            for(int nbr: adj.get(f))
             {
                 in[nbr]--;
-                if(in[nbr]==0)q.offer(nbr);
+                if(in[nbr]==0) q.offer(nbr);
             }
         }
         if(count!=n) return new int[0];
-        int res[] = new int[n];
-        for(int i=0;i<n;i++)
-        {
-            res[i] = op.get(i);
-        }
+        int res[] = new int[op.size()];
+        for(int i=0;i<op.size();i++) res[i] = op.get(i);
         return res;
     }
 }
