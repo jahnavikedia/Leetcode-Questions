0543033faacbbc59ea3a378b6cf5cf6d1cac2df3
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-03-19 21:35:34 -0700

    Time: 2 ms (90.45%), Space: 44.4 MB (35.24%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,44 +1,35 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int dir[][] = {{1,0},{-1,0},{0,1},{0,-1}};
+        int dir[][] = {{-1,0},{0,-1},{1,0},{0,1}};
+        Queue<int[]> q = new LinkedList<>();
         int m = grid.length;
         int n = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]==2)q.offer(new int[]{i,j});
-            }
-        }
+        for(int i=0;i<m;i++) for(int j=0;j<n;j++)if(grid[i][j]==2)q.offer(new int[]{i,j});
         int time=0;
         while(!q.isEmpty())
         {
-            int size=q.size();
-            int temp=0;
+            int size = q.size();
+            int temp = 0;
             for(int i=0;i<size;i++)
             {
                 int p[] = q.poll();
                 for(int d[] : dir)
                 {
-                    int x = d[0]+p[0];
-                    int y = d[1]+p[1];
+                    int x = p[0]+d[0];
+                    int y = p[1]+d[1];
                     if(x>=0 && y>=0 && x<m && y<n && grid[x][y]==1)
                     {
-                        grid[x][y] = 2;
                         temp=1;
+                        grid[x][y] = 2;
                         q.offer(new int[]{x,y});
                     }
                 }
             }
             time+=temp;
         }
-        for(int row[]:grid)
+        for(int i=0;i<m;i++)
         {
-            for(int val:row)
-            {
-                if(val==1) return -1;
-            }
+            for(int j=0;j<n;j++) if(grid[i][j]==1) return -1;
         }
         return time;
     }
