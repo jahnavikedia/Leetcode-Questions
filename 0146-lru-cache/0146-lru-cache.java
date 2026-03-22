f19e2b8bc7606875edd302b1dae7fedd60f245b2
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-03-21 18:09:45 -0700

    Time: 48 ms (44.61%), Space: 134.1 MB (6.48%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,32 +1,33 @@
 class LRUCache {
-    class Node{
-        int key,value;
+    class Node
+    {
+        int key, val;
         Node prev, next;
-        Node(int key, int value)
+        Node(int key, int val)
         {
             this.key = key;
-            this.value = value;
+            this.val = val;
         }
     }
-
     private final int capacity;
     private Map<Integer,Node> map;
-    private Node head,tail;
-
+    private Node head, tail;
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        map = new HashMap<>();
+        this.map = new HashMap<>();
         head = new Node(-1,-1);
         tail = new Node(-1,-1);
         head.next = tail;
         tail.prev = head;
     }
-    public void remove(Node node){
+
+    private void remove(Node node)
+    {
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
 
-    public void insert(Node node)
+    private void insertAtHead(Node node)
     {
         node.next = head.next;
         node.prev = head;
@@ -39,17 +40,17 @@ class LRUCache {
         {
             Node node = map.get(key);
             remove(node);
-            insert(node);
-            return node.value;
+            insertAtHead(node);
+            return node.val;
         }
         return -1;
     }
     
     public void put(int key, int value) {
         if(map.containsKey(key)) remove(map.get(key));
-        Node newNode = new Node(key,value);
-        insert(newNode);
-        map.put(key,newNode);
+        Node node = new Node(key,value);
+        insertAtHead(node);
+        map.put(key,node);
         if(map.size()>capacity)
         {
             Node lru = tail.prev;
