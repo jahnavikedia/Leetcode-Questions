7ecd8a7fc12bab11127cf4a23ec5523f05da6140
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-03-22 10:26:50 -0700

    Time: 2 ms (98.37%), Space: 48.1 MB (75.7%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,35 +1,15 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        Map<Character,Integer> taskFreq = new HashMap<>();
-        for(char task:tasks)
+        int fre[] = new int[26];
+        for(char task : tasks)
         {
-            taskFreq.put(task, taskFreq.getOrDefault(task,0)+1);
+            fre[task-'A']++;
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());//maxHeap
-        pq.addAll(taskFreq.values());
-        int totalTime = 0;
-        while(!pq.isEmpty())
-        {
-            List<Integer> temp = new ArrayList<>();
-            int cycle = 0;
-            for(int i=0;i<=n;i++)
-            {
-                if(!pq.isEmpty())
-                {
-                    temp.add(pq.poll());
-                    cycle++;
-                }
-            }
-            for(int freq : temp)
-            {
-                if(freq-1>0)
-                {
-                    pq.add(freq-1);
-                }
-            }
-            if(pq.isEmpty()) totalTime = totalTime + cycle;
-            else totalTime = totalTime + n+1;
-        }
-        return totalTime;
+        int max=0;
+        for(int f: fre) max = Math.max(max,f);
+        int countMaxFreq = 0;
+        for(int f:fre) if(f==max)countMaxFreq++;
+        int minTime = (max-1)*(n+1) + countMaxFreq;
+        return Math.max(minTime,tasks.length);
     }
 }
