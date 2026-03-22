22a52fcb08ab36414de9e8e207aa350d53b1702a
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-03-21 22:27:24 -0700

    Time: 31 ms (50.47%), Space: 53.5 MB (87.57%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,28 +1,14 @@
-class Pair{
-    int x, y;
-    double distance;
-    Pair(int x, int y)
-    {
-        this.x = x;
-        this.y = y;
-        this.distance = (x*x)+(y*y);
-    }
-}
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingDouble(p -> p.distance));
-        for(int point[]:points)
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> (b[0]*b[0]+b[1]*b[1]) - (a[0]*a[0]+a[1]*a[1]));
+        for(int point[] : points)
         {
-            pq.offer(new Pair(point[0],point[1]));
+            maxHeap.offer(point);
+            if(maxHeap.size()>k) maxHeap.poll();
         }
         int op[][] = new int[k][2];
-        int i=0;
-        while(i<k)
-        {
-            Pair temp = pq.poll();
-            op[i][0] = temp.x;
-            op[i][1] = temp.y;
-            i++;
+        for(int i=0;i<k;i++){
+            op[i] = maxHeap.poll();
         }
         return op;
     }
