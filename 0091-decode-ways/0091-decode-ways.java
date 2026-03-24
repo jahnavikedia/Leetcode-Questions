5487b7ace3734e304dd46750ad04ff54b8d7ad5a
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-03-23 19:18:27 -0700

    Time: 1 ms (86.53%), Space: 43.5 MB (13.38%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,17 +1,16 @@
 class Solution {
     public int numDecodings(String s) {
-        int n = s.length();
-        if(s.charAt(0)=='0') return 0;
-        int t[] = new int[n+1];
-        t[0] = 1;
-        t[1] = s.charAt(0)=='0'?0:1;
-        for(int i=2;i<=n;i++)
-        {
-            int one = Integer.valueOf(s.substring(i-1,i));
-            int two = Integer.valueOf(s.substring(i-2,i));
-            if(one > 0) t[i]+=t[i-1];
-            if(two>=10 && two<=26) t[i]+=t[i-2];
-        }
-        return t[n];
+      int n = s.length();
+      int t[] = new int[n+1];
+      t[0] = 1;
+      t[1] = s.charAt(0)=='0'?0:1;
+      for(int i=2;i<=n;i++)
+      {
+        int one = Integer.valueOf(s.substring(i-1,i));
+        int two = Integer.valueOf(s.substring(i-2,i));
+        if(one>0) t[i]+= t[i-1];
+        if(two>=10 && two<=26) t[i]+=t[i-2];
+      }  
+      return t[n];
     }
 }
