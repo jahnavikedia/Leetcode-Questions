bd41d6163654d9d54fe63ed38766c2b43f2b6e5c
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-03-25 16:48:53 -0700

    Time: 14 ms (62.05%), Space: 47.9 MB (60.69%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -11,49 +11,47 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if (root == null) return "";
-        StringBuilder op = new StringBuilder();
+        if(root==null) return "";
+        StringBuilder sb = new StringBuilder();
         Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
+        q.offer(root);
         while(!q.isEmpty())
         {
             TreeNode curr = q.poll();
-            if(curr == null)
+            if(curr == null) sb.append("N,");
+            else
             {
-                op.append("# ");
-                continue;
+                sb.append(curr.val).append(",");
+                q.offer(curr.left);
+                q.offer(curr.right);
             }
-            op.append(curr.val+" ");
-            q.add(curr.left);
-            q.add(curr.right);
         }
-        return new String(op);
+        return sb.toString(); 
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if(data == "") return null;
-        Queue<TreeNode> q = new LinkedList<>();
-        String values[] = data.split(" ");
+        if(data.isEmpty()) return null;
+        String values[] = data.split(",");
         TreeNode root = new TreeNode(Integer.parseInt(values[0]));
-        int i=1;
-        q.add(root);
-        while(!q.isEmpty())
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int idx=1;
+        while(!q.isEmpty() && idx<values.length)
         {
-            TreeNode parent = q.poll();
-            if(!values[i].equals("#"))
+            TreeNode curr = q.poll();
+            if(!values[idx].equals("N"))
             {
-                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
-                parent.left = left;
-                q.add(left);
+                curr.left = new TreeNode(Integer.parseInt(values[idx]));
+                q.offer(curr.left);
             }
-            if(!values[++i].equals("#"))
+            idx++;
+            if(!values[idx].equals("N"))
             {
-                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
-                parent.right = right;
-                q.add(right);
+                curr.right = new TreeNode(Integer.parseInt(values[idx]));
+                q.offer(curr.right);
             }
-            i++;
+            idx++;
         }
         return root;
     }
