1d3405fd8325d1045e82a1e58ba0d6592d47f3b4
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-04-16 19:57:40 -0700

    Time: 2 ms (99.59%), Space: 44.6 MB (61.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
+        if (s.length() != t.length()) return false;
         int board[] = new int[26];
-        for(char ch: s.toCharArray()) board[ch - 'a']++;
+        for(char ch : s.toCharArray()) board[ch-'a']++;
         for(char ch : t.toCharArray()) board[ch-'a']--;
-        for(int ch : board) if(ch!=0) return false;
+        for(int i : board) if(i>0) return false;
         return true;
     }
 }
