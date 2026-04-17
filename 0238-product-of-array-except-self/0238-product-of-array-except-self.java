91e3e1e232cff0b2f5b6d7d2a330447c84fd4a52
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-04-16 20:40:37 -0700

    Time: 2 ms (95.12%), Space: 72.4 MB (8.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -5,7 +5,7 @@ class Solution {
         op[0] = 1;
         for(int i=1;i<n;i++) op[i] = op[i-1]*nums[i-1];
         int temp = nums[n-1];
-        for(int i=n-2;i>=0;i--) 
+        for(int i=n-2;i>=0;i--)
         {
             op[i] = op[i] * temp;
             temp = temp * nums[i];
