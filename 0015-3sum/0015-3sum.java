33f53f261cc51adbdea57260d263098bfb071c8d
Jahnavi Kedia <48180613+jahnavikedia@users.noreply.github.com>
2026-04-18 09:37:52 -0700

    Time: 29 ms (92.91%), Space: 58.9 MB (90.88%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -14,12 +14,12 @@ class Solution {
                 int sum = nums[j]+nums[k];
                 if(sum==target)
                 {
-                    op.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    while(j<k && nums[j]==nums[j+1])j++;
-                    while(j<k && nums[k]==nums[k-1]) k--;
+                    op.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    while(j<k && nums[j]==nums[j+1]) j++;
+                    while(j<k && nums[k] == nums[k-1]) k--;
                     j++;k--;
                 }
-                else if(sum<target)j++;
+                else if(sum<target) j++;
                 else k--;
             }
         }
